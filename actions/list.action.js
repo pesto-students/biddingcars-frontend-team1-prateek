32b97649d95061c85458a0ee0f6e7645ab5ea890
AmanDas2000 <das.aman45@gmail.com>
2022-10-12 10:56:12 +0530

    logo, car resistration flow preview and back buttons and toast

--- actions/list.action.js
+++ actions/list.action.js
@@ -1,5 +1,6 @@
 import { listConstants } from "./constants";
 import axios from "../helpers/axios";
+import { toast } from 'react-toastify';
 
 
 export const listCar = (form) => async (dispatch) => {
@@ -8,10 +9,13 @@ export const listCar = (form) => async (dispatch) => {
       const res = await axios.post(`/cars/add`,form);
       if (res.status === 200) {
         dispatch({ type: listConstants.POST_LIST_SUCCESS, payload: res.data });
+        toast('Car resistered for approval !', { type: 'success' })
       } else {
         dispatch({ type: listConstants.POST_LIST_FAILURE });
+        toast('Registration unsuccessfull', { type: 'error' })
       }
     } catch (err) {
       dispatch({ type: listConstants.POST_LIST_FAILURE });
+      toast('Registration unsuccessfull', { type: 'error' })
     }
   };
