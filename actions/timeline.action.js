83b7fe5bd7d1d3a95a9fed9384c01d587948cdd0
AmanDas2000 <das.aman45@gmail.com>
2022-10-26 16:54:18 +0530

    bidding flow 1.0

--- actions/timeline.action.js
+++ actions/timeline.action.js
@@ -7,6 +7,7 @@ export const getTimeline = () => async (dispatch) => {
     dispatch({ type: timelineConstants.GET_TIMELINE_REQUEST });
     const res = await axios.get(`/cars`);
     if (res.status === 200) {
+      // console.log(res.data)
       dispatch({ type: timelineConstants.GET_TIMELINE_SUCCESS, payload: res.data });
       // toast('Live Auctions loaded', { type: 'success' })
     } else {
@@ -15,6 +16,24 @@ export const getTimeline = () => async (dispatch) => {
     }
   } catch (err) {
     dispatch({ type: timelineConstants.GET_TIMELINE_FAILURE });
+    toast('Something went wrong.', { type: 'error' })
+  }
+};
+
+export const getHistory = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: timelineConstants.GET_HISTORY_REQUEST });
+    const res = await axios.get(`/cars/${id}/history`);
+    if (res.status === 200) {
+      // console.log(res.data)
+      dispatch({ type: timelineConstants.GET_HISTORY_SUCCESS, payload: res.data });
+      // toast('Live Auctions loaded', { type: 'success' })
+    } else {
+      dispatch({ type: timelineConstants.GET_HISTORY_FAILURE });
+      toast('Something went wrong.', { type: 'error' })
+    }
+  } catch (err) {
+    dispatch({ type: timelineConstants.GET_HISTORY_FAILURE });
     toast('Something went wrong.', { type: 'error' })
   }
 };
